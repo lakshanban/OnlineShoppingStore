dbac85d402eac9fd14b4036fa66f6b391db7189c
EshanKavinda <52734414+EshanKavinda@users.noreply.github.com>
2020-05-02 00:03:04 +0530

    Admin panel added

---
+++ onlineshopping/src/Components/Admin Panel/AdminBottomNavigation.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import FaceIcon from '@material-ui/icons/Face';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import ProductsPanel from "./SubPanels/ProductsPanel";
+import Statistics from "./SubPanels/Statistics";
+import UserPanels from "./SubPanels/UserPanels";
+import Link from "@material-ui/core/Link";
+
+const useStyles = makeStyles({
+    root: {
+        // width: 500,
+    },
+});
+
+
+
+function SimpleBottomNavigation(props) {
+
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+    //console.log(value);
+
+    if (value == 1){
+        //this.history.pushState({},"","");
+        //window.location.href = "new";
+    }else if (value == 2){
+
+    }else if (value == 3){
+
+    }
+
+    return (
+        <BottomNavigation
+            value={value}
+            onChange={(event, newValue) => {
+
+                props.changepage(newValue)
+
+            }}
+            showLabels
+            className={classes.root}
+        >
+            <BottomNavigationAction label="Admin panel" icon={<SupervisorAccountIcon/>} />
+            <BottomNavigationAction label="Products" icon={<ShoppingCartIcon/>} />
+            <BottomNavigationAction label="Statistics" icon={<AttachMoneyIcon/>} />
+            <BottomNavigationAction label="User panel" icon={<FaceIcon />} />
+
+        </BottomNavigation>
+    );
+}
+
+
+class AdminBottomNavigation extends Component {
+
+    constructor(props){
+        super(props)
+    }
+
+
+    render() {
+        return (
+            <div className="fixed-bottom" style={{marginBottom:5}}>
+                <SimpleBottomNavigation changepage={this.props.changepage}/>
+            </div>
+
+
+        );
+    }
+}
+
+export default AdminBottomNavigation;
