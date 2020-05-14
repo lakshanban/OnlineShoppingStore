79272d099862e22ca860696ea415238f269dfc87
EshanKavinda <52734414+EshanKavinda@users.noreply.github.com>
2020-05-15 04:13:42 +0530

    rating and comment interface

--- onlineshopping/src/Components/LoggeHome/LoggedHome.js
+++ onlineshopping/src/Components/LoggeHome/LoggedHome.js
@@ -11,67 +11,53 @@ import ImageShow from "../Product/ImageShow";
 
 export default function LoggedHome(props) {
 
+    const [user,setUser] = useState({});
 
-      const [user,setUser]=useState({});
+    const products=[
+        {name:'T shirt',price:100,description:"description"},
+        {name:'T shirt',price:100,description:"A dress (also known as a frock or a gown) is a garment traditionally worn by women or girls consisting"},
+        {name:'T shirt',price:100,description:"A dress (also known as a frock or a gown) is a garment traditionally worn by women or girls consisting"},
+        {name:'T shirt',price:100,description:"A dress (also known as a frock or a gown) is a garment traditionally worn by women or girls consisting"},
+        {name:'T shirt',price:100,description:"A dress (also known as a frock or a gown) is a garment traditionally worn by women or girls consisting"}
+        ]
+    //let products = [];
 
 
-    const products=[{name:'T shirt',price:100,description:"A dress (also known as a frock or a gown) is a garment traditionally worn by women or girls consisting"},{name:'T shirt',price:100,description:"A dress (also known as a frock or a gown) is a garment traditionally worn by women or girls consisting"},{name:'T shirt',price:100,description:"A dress (also known as a frock or a gown) is a garment traditionally worn by women or girls consisting"},{name:'T shirt',price:100,description:"A dress (also known as a frock or a gown) is a garment traditionally worn by women or girls consisting"},{name:'T shirt',price:100,description:"A dress (also known as a frock or a gown) is a garment traditionally worn by women or girls consisting"}]
+
+    // axios.get('http://localhost:8080/getallproducts')
+    //     .then(res=> {
+    //
+    //     });
+
+
+    // item.pro.map(value => {
+    //     console.log(value.pdiscount);
+    // });
 
     function update(){
-
-        axios.post('http://localhost:8080/userget',{username:props.user}).then(res=>{
-
+            axios.post('http://localhost:8080/userget',{username:props.user}).then(res=>{
             setUser(res.data);
-
             console.log(res.data)
-
-
         })
-
-
     }
-
-
 
     return(
 
-
-
-
         <div>
         <ComplexNavigationBar dispatch={props.dispatch} user={user}/>
-
-
             <Container maxWidth={"xl"} style={{marginLeft:'5%'}}>
-
-
-
                 <Grid container spacing={1} style={{display:"flex",marginTop:'50px'}}>
-
                     {
                         products.map(product=>{
-
-                            return <Grid container item xs={4} spacing={4} style={{margin:'00px'}} className="gridItem">
-
+                            return (
+                            <Grid container item xs={4} spacing={4} style={{margin:'00px'}} className="gridItem">
                                 <Post product={product} dispatch={props.dispatch} setproduct={props.setproduct}/>
-
                             </Grid>
-
-
+                            )
                     })
-
-
-
                     }
-
                 </Grid>
-
-
-
             </Container>
-
-
-
         </div>
         )
 
--- onlineshopping/src/Components/LoggeHome/Post.js
+++ onlineshopping/src/Components/LoggeHome/Post.js
@@ -26,8 +26,6 @@ export default function Post(props){
 
     }
 
-
-
     return (
         <div>
 
