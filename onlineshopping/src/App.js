b1c101adf3cc52765983c2a6abf9122d158c319d
EshanKavinda <52734414+EshanKavinda@users.noreply.github.com>
2020-05-22 21:54:09 +0530

    commit

--- onlineshopping/src/App.js
+++ onlineshopping/src/App.js
@@ -11,17 +11,12 @@ import Product from "./Components/Product/Product";
 import ProductsData from "./Components/Store Manager/ProductsData";
 import DetailForm from "./Components/PaymentGateway/DetailForm";
 import NonLoggedHome from "./Components/NonLoggedHome/NonLoggedHome";
+import CheckoutForm from "./Components/PaymentGateway/CheckoutForm";
 
 function App() {
   return (
     <div className="App">
        <Test/>
-<<<<<<< HEAD
-=======
-       <DetailForm/>
-       {/*<ProductsData/>*/}
-       {/*<NonLoggedHome/>*/}
->>>>>>> fcb3fd60c358ee150ab513f4e3df1d11b68c4466
     </div>
   );
 }
